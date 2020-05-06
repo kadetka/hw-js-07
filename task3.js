88eb42af9c7179c60d9159880cabe7b4ee643ca5
Violetta <40901389+kadetka@users.noreply.github.com>
2020-05-06 21:28:14 +0300

    fix task3, task4

--- task3.js
+++ task3.js
@@ -9,50 +9,48 @@
 получить строку-перевертыш
 */
 
-const moduleString = function () {
-    let str;
+const moduleString = (function () {
+  let str;
 
-    function setString (string) {
-        if(string === undefined){
-            str = '';
-        }else if(typeof string === 'number'){
-            str = toString(string);
-        }else{
-            str = string
-        }
+  function setString(string) {
+    if (string === undefined) {
+      str = "";
+    } else if (typeof string === "number") {
+      str = toString(string);
+    } else {
+      str = string;
+    }
 
-        return str;
-    };
+    return str;
+  }
 
-    function getString () {
-        
-        return str;
-    };
+  function getString() {
+    return str;
+  }
 
-    function getStringLength () {
-        
-        return str.length;
-    };
+  function getStringLength() {
+    return str.length;
+  }
 
-    function getFlipString () {
-        let flipString = '';
+  function getFlipString() {
+    let flipString = "";
 
-        for(let i = str.length - 1; i >= 0; i--){
-            flipString += str[i];
-        };
+    for (let i = str.length - 1; i >= 0; i--) {
+      flipString += str[i];
+    }
 
-        return flipString;
-    };
+    return flipString;
+  }
 
-    return {
-        setString,
-        getString,
-        getStringLength,
-        getFlipString
-    };
-}();
+  return {
+    setString,
+    getString,
+    getStringLength,
+    getFlipString,
+  };
+})();
 
-console.log(moduleString.setString('abcd'));
+console.log(moduleString.setString("abcd"));
 console.log(moduleString.getString());
 console.log(moduleString.getStringLength());
 console.log(moduleString.getFlipString());
--- task4.js
+++ task4.js
@@ -1,57 +1,50 @@
 // Создайте модуль “калькулятор”, который умеет складывать, умножать, вычитать, делить и возводить в степень. Конечное значение округлить до двух знаков после точки (значение должно храниться в обычной переменной, не в this).
 
-const calculateModul = function () {
-    let number;
+const calculateModul = (function () {
+  let number;
 
-    function setNumber (num) {
-       
-       return number = num;
-    };
+  function setNumber(num) {
+    return (number = num);
+  }
 
-    function addNumber (num) {
-        
-        return number += num;
+  function addNumber(num) {
+    return (number += num);
+  }
 
-    };
+  function multiplyNumber(num) {
+    return (number *= num);
+  }
 
-    function multiplyNumber (num) {
-        
-        return number *= num;
-    };
+  function subtractNumber(num) {
+    return (number -= num);
+  }
 
-    function subtractNumber (num) {
+  function divideNumber(num) {
+    return (number /= num);
+  }
 
-        return number -= num;
-    };
+  function degreeNumber(num) {
+    number = Math.pow(number, num);
 
-    function divideNumber (num) {
+    return number;
+  }
 
-        return number /= num;
-    };
+  function getNumber() {
+    number = number.toFixed(2);
 
-    function degreeNumber (num) {
-        number = Math.pow(number, num);
+    return number;
+  }
 
-        return number;
-    };
-
-    function getNumber () {
-        number = number.toFixed(2);
-
-        return number;
-    }
-
-    return {
-        setNumber,
-        addNumber,
-        multiplyNumber,
-        subtractNumber,
-        divideNumber,
-        degreeNumber,
-        getNumber
-    };
-}();
-
+  return {
+    setNumber,
+    addNumber,
+    multiplyNumber,
+    subtractNumber,
+    divideNumber,
+    degreeNumber,
+    getNumber,
+  };
+})();
 
 console.log(calculateModul.setNumber(10));
 console.log(calculateModul.addNumber(5));
@@ -60,4 +53,3 @@ console.log(calculateModul.subtractNumber(10));
 console.log(calculateModul.divideNumber(4));
 console.log(calculateModul.degreeNumber(3));
 console.log(calculateModul.getNumber());
-
